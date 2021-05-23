613e803e2cdf115d5c330be3c901d0cb584da70c
mipsmonsta <yaojintham@gmail.com>
2021-05-23 17:47:49 +0800

    Added javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+document.body.addEventListener("change", function (e) {
+  let target = e.target;
+
+  switch (target.id) {
+    case "first_toggle":
+      console.log("first");
+      break;
+    case "second_toggle":
+      console.log("second");
+      break;
+    case "third_toggle":
+      console.log("third");
+      break;
+  }
+});
