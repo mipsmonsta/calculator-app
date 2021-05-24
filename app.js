5287fe3e588971a539eb8dbc339a77fb3ab111a2
mipsmonsta <yaojintham@gmail.com>
2021-05-24 12:28:45 +0800

    Added demo click listener for buttons

--- app.js
+++ app.js
@@ -14,4 +14,30 @@ document.body.addEventListener("change", function (e) {
   }
 });
 
+document.body.addEventListener("click", (e) => {
+  let target = e.target;
 
+  switch (target.id) {
+    case "btns-9":
+    case "btns-8":
+    case "btns-7":
+    case "btns-6":
+    case "btns-5":
+    case "btns-4":
+    case "btns-3":
+    case "btns-2":
+    case "btns-1":
+    case "btns-dot":
+    case "btns-0":
+    case "btns-divide":
+    case "btns-multiply":
+    case "btns-reset":
+    case "btns-equal":
+    case "btns-del":
+      let element = document.querySelector(`#${e.target.id}`);
+      console.log(`Calculator Btns Clicked ${element.textContent}`);
+
+    default:
+      break;
+  }
+});
